3ee15d190dc9e5c631a91eb9dcb5b6d2e3841d35
irwanputrasetia <irwanputrastya@gmail.com>
2017-11-07 16:49:47 +0700

    Pengcodingan dengan menggunakan percabangan if , coding perhitungan aritmatika

--- soal2.cpp
+++ soal2.cpp
@@ -8,4 +8,26 @@ int main () {
 
 	cout << "Pilih Bidang : ";
 	cin >> bangun;
+	
+		if (bangun == 'P' || bangun == 'p') {	
+		cout << "   Mengitung luas persegi panjang \n";	
+		cout << "   Masukkan panjangnya : ";
+		cin >> p;
+		cout << "   Masukkan lebarnya   : ";
+		cin >> l;
+		Luas = p*l;
+		cout << "   Luas persegi panjang : "<< Luas<<endl;
+	}
+	else if (bangun == 'S' || bangun == 's') {
+		cout << "   Mengitung luas segitiga \n";	
+		cout << "   Masukkan alasnya   : ";
+		cin >> a;
+		cout << "   Masukkan tingginya : ";
+		cin >> t;
+		Luas = 0.5*a*t;
+		cout << "   Luas persegi panjang : "<< Luas<<endl;
+	}	
+	else{
+		cout<<"Maaf, Anda Salah Memilih"<<endl;
+	}
 }
