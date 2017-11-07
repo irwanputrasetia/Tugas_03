9802a2b94a400fdfd8f7b2d40b406293e1026c4c
irwanputrasetia <irwanputrastya@gmail.com>
2017-11-07 17:00:03 +0700

    pembuatan variabel dan memasukkan variabel

---
+++ soal3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main () {
+	int gaji,jam,lembur,gatot,pajak;
+	
+	cout << "Masukkan gaji pegawai : ";
+	cin >> gaji;
+	cout << "Masukkan jam kerja    : ";
+	cin >> jam;
+}
