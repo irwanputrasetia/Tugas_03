2dd7c005841e1abb75d5a3dae4ca155260e6b15c
irwanputrasetia <irwanputrastya@gmail.com>
2017-11-08 15:53:43 +0700

    ada perubahan , penambahan && (AND)

--- soal5.cpp
+++ soal5.cpp
@@ -14,7 +14,7 @@ int main () {
  	if (berat<tinggi/2.5) {
   	cout << "Hasil : Kurus " << endl;
  }
- 	else if  (tinggi/2.5 <= berat <= tinggi/2.3) {
+ 	else if  (tinggi/2.5 <= berat && berat <= tinggi/2.3) {
   	cout <<  "Hasil : Normal" << endl; 
  }
  
